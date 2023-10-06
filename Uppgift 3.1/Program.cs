e5aef69607d724fe39485061ff9ae4bd316390d9
sere06001 <sere06001@utb.vaxjo.se>
2023-10-07 01:39:18 +0200

    Add project files.

---
+++ Uppgift 3.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Uppgift3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder >= 16 && ålder <= 19)
+            {
+                Console.WriteLine("Du får delta i tävlingen.");
+            }
+            else if (ålder > 19)
+            {
+                Console.WriteLine("Du är för gammal för att delta.");
+            }
+            else
+            {
+                Console.WriteLine("Du är för ung för att delta.");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
